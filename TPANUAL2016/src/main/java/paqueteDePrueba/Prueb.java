d36c4cedd36ca8382a26844bd2f2b3f1f3c0d7c9
Lhoms <lucashoms@gmail.com>
2016-04-10 21:25:22 -0300

    Prueba + Directorios + Pom

---
+++ TPANUAL2016/src/main/java/paqueteDePrueba/Prueb.java
@@ -0,0 +1,11 @@
+package paqueteDePrueba;
+
+public class Prueb {
+	
+	
+	public void probar() {
+		System.out.println("Algo en pantalla\n");
+	}
+	
+
+}
---
+++ TPANUAL2016/src/main/java/tests/MainTest.java
@@ -0,0 +1,17 @@
+package tests;
+
+import paqueteDePrueba.Prueb;
+
+public class MainTest {
+	
+	public static void main(String[] args) {
+		
+		Prueb prueba;
+		prueba = new Prueb();
+		
+		prueba.probar();
+		
+		
+	}
+
+}
